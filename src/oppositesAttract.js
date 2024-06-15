458bd66bc13dc1b71e0c068efa5a569871d62eac
Bego Blanco <begoblanfer@gmail.com>
2024-06-15 12:21:30 +0200

    function added

---
+++ src/oppositesAttract.js
@@ -0,0 +1,20 @@
+function isLove(petalsFlower1, petalsFlower2) {
+  if (!Number.isInteger(petalsFlower1) || !Number.isInteger(petalsFlower2)) {
+    return "The number of petals must be and integer";
+  }
+  if (
+    (petalsFlower1 % 2 == 0 && petalsFlower2 % 2 !== 0) ||
+    (petalsFlower1 % 2 !== 0 && petalsFlower2 % 2 == 0)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isLove(1, 4)); //true
+console.log(isLove(2, 2)); //false
+console.log(isLove(0, 1)); //true
+console.log(isLove(0, 0)); //false
+console.log(isLove(1.5, 4)); //The number of petals must be and integer
+console.log(isLove("miau", 4)); //The number of petals must be and integer
